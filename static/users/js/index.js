98bd191cf0e1406e17e2eeb15b2de0ac183c9b75
zhongyuan <zhongyuan12345@qq.com>
2020-12-19 11:12:30 +0800

    暂存一下

--- static/users/js/index.js
+++ static/users/js/index.js
@@ -8,14 +8,15 @@ let app = new Vue({
         default_active: "1",
         login_user: {
             name: "钟源",
-            id: ""
+            username: ""
         },
         address: {
-            all: [],
+            all: [{addressDetail: "当代国际花园", receiverName: "钟源", phoneNum: "18186113076"}],
             current: {},
             temporal_coor: {},  // 选择的经纬度暂存于此
             search_keyword: '',
             map: 0,
+            BMap: undefined,
             edit_mode: false,
             mapInfo: {
                 center: {lng: 114.372042, lat: 30.544861},
@@ -23,6 +24,37 @@ let app = new Vue({
             },
             addVisible: false,
             mapVisible: false,
+            rules: {
+                province: [
+                    {required: true, message: '请输入省份', trigger: 'change'},
+                ],
+                receiverName: [
+                    {required: true, message: '请输入省份', trigger: 'change'},
+                ],
+                city: [
+                    {required: true, message: '请输入市区', trigger: 'change'},
+                ],
+                addressDetail: [
+                    {required: true, message: '请输入详细地址', trigger: 'change'},
+                ],
+                phoneNum: [
+                    {required: true, message: '请输入联系方式', trigger: 'change'},
+                ],
+            }
+        },
+        order: {
+            all: [],
+            current: {},
+            temporal_coor: {},  // 选择的经纬度暂存于此
+            search_keyword: '',
+            map: 0,
+            mapInfo: {
+                center: {lng: 114.372042, lat: 30.544861},
+                zoom: 16
+            },
+            addVisible: false,
+            mapVisible: false,
+            tobereceived_orders: [],
             rules: {
                 province: [
                     {required: true, message: '请输入省份', trigger: 'change'},
@@ -38,13 +70,13 @@ let app = new Vue({
                 ],
             }
         },
-
-        all_orders: [],
-        tobereceived_orders: [],
-
+        seller: {
+            all: [{username: "哈哈哈", name: "你好", city: "武汉"}]
+        },
 
     },
     methods: {
+        // 公共部分
         hideAll() {
             document.getElementById('address_manage').style.display = 'none';
             document.getElementById('all_order').style.display = 'none';
@@ -84,28 +116,131 @@ let app = new Vue({
                     break;
             }
         },
+
+        // 地址部分
+        address_addoverlay(lng, lat) {
+            var myMarker = new BMap.Marker(new BMap.Point(lng, lat));
+            this.address.map.addOverlay(myMarker);
+        },
+        address_zoom_and_skip(encoded_address) {
+            this.address.mapInfo.center = encoded_address;
+            this.address_addoverlay(encoded_address.lng, encoded_address.lat);
+            this.address.temporal_coor.addressLon = encoded_address.lng;
+            this.address.temporal_coor.addressLat = encoded_address.lat;
+        },
         address_showMap() {
             this.address.mapVisible = true;
+            if (this.address.BMap !== undefined) {
+                let myGeo = new this.address.BMap.Geocoder();
+                let encoded_address = undefined;
+                if (this.address.current.city !== undefined && this.address.current.addressDetail !== undefined) {
+                    myGeo.getPoint(this.address.current.addressDetail, function (point) {
+                        if (point) {
+                            encoded_address = {lng: point.lng, lat: point.lat};
+                        } else {
+                            encoded_address = {lng: 114.372042, lat: 30.544861};
+                        }
+                        app.address_zoom_and_skip(encoded_address);
+                    }, this.address.current.city);
+                } else {
+                    encoded_address = {lng: 114.372042, lat: 30.544861};
+                    this.address_zoom_and_skip(encoded_address);
+                }
+            }
         },
         // 地图初始化时调用
         address_mapHandler({BMap, map}) {
             this.address.map = map;   //将map变量存储在全局
+            this.address.BMap = BMap;
+            let myGeo = new BMap.Geocoder();
+            let encoded_address = undefined;
+            if (this.address.current.city !== undefined && this.address.current.addressDetail !== undefined) {
+                myGeo.getPoint(this.address.current.addressDetail, function (point) {
+                    if (point) {
+                        encoded_address = {lng: point.lng, lat: point.lat};
+                    } else {
+                        encoded_address = {lng: 114.372042, lat: 30.544861};
+                    }
+                    app.address_zoom_and_skip(encoded_address);
+                }, this.address.current.city);
+            } else {
+                encoded_address = {lng: 114.372042, lat: 30.544861};
+                this.address_zoom_and_skip(encoded_address);
+            }
+        },
+        address_closemap() {
+            this.address.temporal_coor = {};
+            this.address.map.clearOverlays();
+            this.address.mapVisible = false;
         },
         // 地图坐标选取
         address_pointPick(e) {
             this.address.map.clearOverlays();
-            var myMarker = new BMap.Marker(new BMap.Point(e.point.lng, e.point.lat));
-            this.address.map.addOverlay(myMarker);
+            this.address_addoverlay(e.point.lng, e.point.lat);
             this.address.temporal_coor = {addressLon: e.point.lng, addressLat: e.point.lat};
         },
-        addAddress() {
-            this.address.current = {id: this.address.all.length + 1};
+        address_location_select() {
+            let myGeo = new this.address.BMap.Geocoder();
+            myGeo.getLocation(new this.address.BMap.Point(this.address.temporal_coor.addressLon, this.address.temporal_coor.addressLat), function (result) {
+                if (result) {
+                    let addComp = result.addressComponents;
+                    app.address.current.province = addComp.province;
+                    app.address.current.city = addComp.city;
+                    app.address.current.addressDetail = result.address;
+                    app.address.current.addrsssLon = app.address.temporal_coor.addressLon;
+                    app.address.current.addrsssLat = app.address.temporal_coor.addressLat;
+                    app.address.mapVisible = false;
+                }
+            });
+        },
+        address_add() {
+            this.address.current = {};
+            let newAddress = Object.assign([], this.address.all);
+            let sort = newAddress.sort((a, b) => a.id - b.id);
+            let i = 0;
+            try {
+                sort.forEach(address => {
+                    i++;
+                    if (address.id !== i) {
+                        this.address.current.id = i;
+                        throw new Error("ending");  // 退出循环
+                    }
+                });
+                this.address.current.id = i + 1;
+            } catch (e) {
+                return 0;
+            }
             this.address.edit_mode = false;
             this.address.addVisible = true;
         },
-        queryAddress() {
+        address_query() {
 
-        }
+        },
+        address_refresh() {
+
+        },
+
+        // 订单部分
+        order_query() {
+
+        },
+        order_add() {
+            this.order.current = {};
+            let timestamp = (new Date()).valueOf();
+            let random_num = Math.floor(Math.random() * 100);
+            this.order.current.id = timestamp.toString() + random_num.toString();
+
+            this.order.addVisible = true;
+        },
+        order_refresh() {
+
+        },
+        order_check_if_changeable(order) {
+
+        },
+        order_check_if_returnable(order) {
+
+        },
     }
 });
 
